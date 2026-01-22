0019cb924564f30c8c67a8b4430cf20f38e1bab8
Abhinav <py.abhinav2@gmail.com>
2026-01-21 21:17:16 -0800

    Initial Commit

---
+++ Anikin/AnikinApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnikinApp.swift
+//  Anikin
+//
+//  Created by Abhinav Prasad Yasaswi on 1/21/26.
+//
+
+import SwiftUI
+
+@main
+struct AnikinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
